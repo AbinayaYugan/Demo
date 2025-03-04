5e90cbf487b49c66144a1fb1813928500d8bb8cb
Abinaya <abinayanachiyappan37@gmail.com>
2025-03-04 10:35:03 +0530

    Added new line

--- axess/Add.java
+++ axess/Add.java
@@ -5,6 +5,7 @@ public class Add {
 	public static void main(String[] args) {
 		
 		System.out.println("Push the code into Remote");
+		System.out.println("Pushed the code 2 into Remote");
 		
 	}
 
